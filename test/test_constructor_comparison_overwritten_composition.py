38a7a33af6f269420511520dafbe6ea0c5674d1e
Lihong Zhang <zlhlianne@163.com>
2019-12-09 02:12:09 -0500

    updating test-lihong Dec 2:12AM

--- test/test_constructor_comparison_overwritten_composition.py
+++ test/test_constructor_comparison_overwritten_composition.py
@@ -1,4 +1,5 @@
-from AutoDiff.ForwardAD import Var, MultiFunc
+from AutoDiff.ForwardAD import Var
+from AutoDiff.ForwardAD import MultiFunc
 import numpy as np
 import math
 
@@ -23,28 +24,28 @@ def test_constructor():
     # vector input and scalar output    
     def test_vec_scalar():
         z = 2*x+y**2
-        assert z.get_value() == 3.0
+        assert z.get_value() == 6.0
         assert type(z.get_value()) == float
         assert z._get_derivative_of(x) == 2.0
         assert z._get_derivative_of(y) == 4.0
-        assert type(z._get_derivative_of(x)) == 2.0
-        assert type(z._get_derivative_of(y)) == 4.0
+        assert type(z._get_derivative_of(x)) == float
+        assert type(z._get_derivative_of(y)) == float
         assert z.get_der([x,y]) == [2.0, 4.0]
         assert type(z.get_der([x,y])) == list
         
     # scalar input and vector output
     def test_scalar_vector():
-        vec = MultiFunc[x, x**2] 
-        assert vec.get_value() == [1.0, 2.0]
+        vec = MultiFunc([x, x**2]) 
+        assert vec.get_value() == [1.0, 1.0]
         assert vec.get_der([x]) == [[1.0], [2.0]]
         assert type(vec.get_value()) == list
         assert type(vec.get_der([x])) == list
         
     # vector input and vector output
     def test_vector_vector():
-        z = MultiFunc[x+y, y**2]
+        z = MultiFunc([x+y, y**2])
         assert z.get_value() == [3.0, 4.0]
-        assert z.get_der([x,y]) == [[1.0, 0], [1.0, 4.0]]
+        assert z.get_der([x,y]) == [[1.0, 1.0], [0, 4.0]]
         ## partial derivative
         assert type(z.get_value()) == list
         assert type(z.get_der([x,y])) == list
@@ -122,7 +123,7 @@ def test_scalar_scalar():
 
             z3 = (x/0.5)/0.1
             assert z3.get_value() == 80.0
-            assert z3.get_der() == [20.0]
+            assert np.round(z3.get_der(), 10) == np.round([20.0], 10)
 
         def test_rdiv():
             z1 = 8.0/x
@@ -174,7 +175,7 @@ def test_scalar_scalar():
         m1 = x + x
         m2 = 2*x
         z1 = x*4.0/2.0
-        z2 = z*2.0
+        z2 = x*2.0
             
         def test_eq():
             assert not x == y
@@ -268,10 +269,10 @@ def test_vector_scalar():
         def test_subtract():
             z1 = z-x-y-1
             assert z1.get_value() == -3.0
-            assert z1.get_der() == [1.0, -1.0, -1.0]
-            assert z1._get_derivative_of(x) == 1.0
+            assert z1.get_der([x, y, z]) == [-1.0, -1.0, 1.0]
+            assert z1._get_derivative_of(x) == -1.0
             assert z1._get_derivative_of(y) == -1.0
-            assert z1._get_derivative_of(z) == -1.0
+            assert z1._get_derivative_of(z) == 1.0
 
             # z not modified
             assert z.get_value() == Var(5.0).get_value()
@@ -301,8 +302,8 @@ def test_vector_scalar():
 
         def test_div():
             z1 = (z/x)/10
-            assert z2.get_value() == 5.0/40.0
-            assert z2.get_der() == [1.0/40.0, -5.0/160.0]
+            assert z1.get_value() == 5.0/40.0
+            assert z1.get_der([x,z]) == [ -5.0/160.0, 1.0/40.0]
             assert z1._get_derivative_of(z) == 1.0/40.0
             assert z1._get_derivative_of(x) == -5.0/160.0
 
@@ -316,16 +317,16 @@ def test_vector_scalar():
         def test_pow():
             z1 = x**y
             assert z1.get_value() == 64.0
-            assert np.round(z1.get_der(),4) == np.round([48.0, 64*np.log(4)], 4)
-            assert np.round(z1._get_derivative_of(x), 4) == np.round(48.0, 4)
-            assert np.round(z1._get_derivative_of(y), 4) == np.round(64*np.log(4), 4)
+            np.testing.assert_array_equal(np.round(z1.get_der([x,y]),10), np.round([48.0, 64*np.log(4)], 10))
+            assert np.round(z1._get_derivative_of(x), 10) == np.round(48.0, 10)
+            assert np.round(z1._get_derivative_of(y), 10) == np.round(64*np.log(4), 10)
 
         def test_rpow():
             z1 = (2**x)**y
             assert z1.get_value() == 4096.0
-            assert np.round(z1.get_der(), 4) == np.round([12288*np.log(2), 16384*np.log(2)], 4)
-            assert np.round(z1._get_derivative_of(x), 4) == np.round(12288*np.log(2), 4)
-            assert np.round(z1._get_derivative_of(y), 4) == np.round(16384*np.log(2), 4)
+            np.testing.assert_array_equal(np.round(z1.get_der(), 10), np.round([12288*np.log(2), 16384*np.log(2)], 10))
+            assert np.round(z1._get_derivative_of(x), 10) == np.round(12288*np.log(2), 10)
+            assert np.round(z1._get_derivative_of(y), 10) == np.round(16384*np.log(2), 10)
 
         test_add()
         test_radd()
@@ -352,15 +353,15 @@ def test_vector_scalar():
             assert z1.__eq__(z2)
             assert not x == y
                 
-            z1 = x*4.0/2.0
-            z2 = z*2.0
-            assert z1 == z2
+            m1 = x*4.0/2.0
+            m2 = x*2.0
+            assert m1 == m2
             
         def test_ne():
             z3 = -z1
             assert not z3 == z2
             
-            x2 = -x1
+            x2 = -x
             x2.get_value() == -4.0
             
             
@@ -405,15 +406,15 @@ def test_vector_scalar():
         def test_1_trig():
             z1 = Var.sin(Var.cos(x))+ Var.sin(y)
             assert np.round(z1.get_value(),10) == -0.8414709848
-            assert np.round(z1.get_der(),10) == [0, 1.0]
-            assert np.round(z1._get_derivative_of(x), 4) == np.round(0, 4)
-            assert np.round(z1._get_derivative_of(y), 4) == np.round(1.0, 4)
+            np.testing.assert_array_equal(np.round(z1.get_der([x,y]), 10), np.round([0, 1.0], 10))
+            assert np.round(z1._get_derivative_of(x), 10) == np.round(0, 10)
+            assert np.round(z1._get_derivative_of(y), 10) == np.round(1.0, 10)
             
 
         def test_2():
             z2 = Var.sin(x**2) + Var.sin(z)
             assert np.round(z2.get_value(), 9) == -0.430301217+1.0
-            assert np.round(z2.get_der(),10) == [-5.6717394031, 0]
+            np.testing.assert_array_equal(np.round(z2.get_der([x,z]),10), np.round([-5.6717394031, 0], 10))
             
         test_1_trig()
         test_2()
@@ -430,61 +431,59 @@ def test_scalar_vector():
         def test_add():
             z1 = y + 1
             assert z1.get_value() == [9.0, 17.0]
-            assert z1.get_der([x]) == [1.0, 8.0]
-            #assert z1._get_derivative_of(x) = 1.0
-            #assert z1._get_derivative_of(x2) = 8.0
+            assert z1.get_der([x]) == [[2.0], [8.0]]
             
             z2 = z + MultiFunc([x, x])
             assert z2.get_value() == [8.0, 12.0]
-            assert z2.get_der([x]) == [2.0, 3.0]
+            assert z2.get_der([x]) == [[2.0], [3.0]]
 
         def test_radd():
             z1 = 2 + y
             assert z1.get_value() == [10.0, 18.0]
-            assert z1.get_der([x]) == [1.0, 8.0]
+            assert z1.get_der([x]) == [[2.0], [8.0]]
 
         def test_subtract():
             z1 = y - z - 1  # = [x, x^2 - 2x]
             assert z1.get_value() == [3.0, 7.0]
-            assert z1.get_der([x]) == [1.0, 6.0]
+            assert z1.get_der([x]) == [[1.0], [6.0]]
 
             # z not modified
             assert z.get_value() == MultiFunc([x, 2*x]).get_value()
 
         def test_rsubtract():
             z1 = 10.0-y-z
-            assert z1.get_value() == [-38.0, -14.0]
-            assert z1.get_der([x]) == [-14.0, 0]
+            assert z1.get_value() == [-2.0, -14.0]
+            assert z1.get_der([x]) == [[-3.0], [-10.0]]
 
         def test_mul():
             z1 = y*z*2
             assert z1.get_value() == [64.0, 256.0]
-            assert z1.get_der([x]) == [32.0, 96.0]
+            assert z1.get_der([x]) == [[32.0], [192.0]]
 
         def test_rmul():
             z1 = 2*y*z
             assert z1.get_value() == [64.0, 256.0]
-            assert z1.get_der([x]) == [32.0, 96.0]
+            assert z1.get_der([x]) == [[32.0], [192.0]]
 
         def test_div():
             z1 = (y/z)/10
             assert z1.get_value() == [0.2, 0.2]
-            assert z1.get_der([x]) == [0, 0.05]
+            assert z1.get_der([x]) == [[0.0], [0.05]]
 
         def test_rdiv():
             z1 = 10/(y/x)
-            assert z1.get_value() == [5.0, 5.0]
-            assert z1.get_der([x]) == [0, -1.25]
+            assert z1.get_value() == [5.0, 2.5]
+            assert z1.get_der([x]) == [[0], [-0.625]]
 
         def test_pow():
             z1 = y**2
             assert z1.get_value() == [64.0, 256.0]
-            assert z1.get_der([x]) == [32.0, 256.0]  #=【8x, 4x^3]
+            assert z1.get_der([x]) == [[32.0], [256.0]]  #=【8x, 4x^3]
 
         def test_rpow():
             z1 = 2**y
             assert z1.get_value() == [256.0, 65536.0]
-            assert np.round(z1.get_der([x]), 4) == np.round([512.0*np.log(2), 524288.0*np.log(2)], 4)
+            np.testing.assert_array_equal(np.round(z1.get_der([x]), 10), np.round([[512.0*np.log(2)], [524288.0*np.log(2)]], 10))
 
         test_add()
         test_radd()
@@ -559,8 +558,8 @@ def test_scalar_vector():
         def test_1_trig():
             f = y.apply(Var.cos)
             z1 = f.apply(Var.sin)
-            assert np.round(z1.get_value(),10) == -0.8414709848
-            assert np.round(z1.get_der([x]),10) == [0, 0]
+            np.testing.assert_array_equal(np.round(z1.get_value(),10), np.round([-0.8414709848, -0.8414709848], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([x]),10), np.round([[0], [0]], 10))
             
 
         '''
@@ -584,7 +583,7 @@ def test_vector_vector():
         p = MultiFunc([y*2, x+2])
         def test_add():
             z1 = z + 1
-            assert z1.get_value() == [3.0, 2.0]
+            assert z1.get_value() == [4.0, 3.0]
             assert z1.get_der([x, y]) == [[1.0, 0], [0, 1.0]]
             assert z1.get_der([x]) == [[1.0], [0]]
             assert z1.get_der([y]) == [[0], [1.0]]
@@ -592,14 +591,14 @@ def test_vector_vector():
             #assert z1._get_derivative_of(x2) = 8.0
             
             z2 = MultiFunc([y, x]) + MultiFunc([x, x])
-            assert z2.get_value() == [5.0, 5.0]
+            assert z2.get_value() == [5.0, 6.0]
             assert z2.get_der([x,y]) == [[1.0, 1.0], [2.0, 0]]
             assert z2.get_der([x]) == [[1.0], [2.0]]
             assert z2.get_der([y]) == [[1.0], [0]]
 
         def test_radd():
             z1 = 1 + z
-            assert z1.get_value() == [3.0, 2.0]
+            assert z1.get_value() == [4.0, 3.0]
             assert z1.get_der([x, y]) == [[1.0, 0], [0, 1.0]]
             assert z1.get_der([x]) == [[1.0], [0]]
             assert z1.get_der([y]) == [[0], [1.0]]
@@ -636,36 +635,36 @@ def test_vector_vector():
             
             z2 = z*p  # = [x*y^2, y(x+2)]
             assert z2.get_value() == [12.0, 10.0]
-            assert z2.get_der([x,y]) == [[4.0, 12.0], [2.0, 5.0]]
+            assert z2.get_der([x,y]) == [[4.0, 6.0], [2.0, 5.0]]
             assert z2.get_der([x]) == [[4.0], [2.0]]
-            assert z2.get_der([y]) == [[12.0], [5.0]]
+            assert z2.get_der([y]) == [[6.0], [5.0]]
 
         def test_rmul():
             z1 = 2*z
             assert z1.get_value() == [6.0, 4.0]
-            assert z1.get_der([x,y] == [[2.0, 0], [0, 2.0]]
-            assert z1.get_der([x]) == [[2.0], [0]]
+            assert z1.get_der([x,y]) == [[2.0, 0], [0, 2.0]]
+            assert z1.get_der([x]) == [[2.0], [0.0]]
             assert z1.get_der([y]) == [[0], [2.0]]
 
         def test_div():
             z1 = z/10
             assert z1.get_value() == [0.3, 0.2]
-            assert z1.get_der() == [[0.1, 0], [0, 0.1]]
+            assert z1.get_der([x,y]) == [[0.1, 0], [0, 0.1]]
             assert z1.get_der([x]) == [[0.1], [0]]
             assert z1.get_der([y]) == [[0], [0.1]]
             
             z2 = p/z
-            assert np.round(z2.get_value(), 4) == np.round([4.0/3, 2.5], 4)
-            assert np.round(z2.get_der([x,y]), 10) == np.round([[-4.0/9, 2.0/3], [0.5, -5.0/4]], 10)
-            assert np.round(z2.get_der([x]),10) == np.round([[-4.0/9], [0.5]], 10)
-            assert np.round(z2.get_der([y]), 10) == np.round([[2.0/3], [-5.0/4]], 10)
+            np.testing.assert_array_equal(np.round(z2.get_value(), 10), np.round([4.0/3, 2.5], 10))
+            np.testing.assert_array_equal(np.round(z2.get_der([x,y]), 10), np.round([[-4.0/9, 2.0/3], [0.5, -5.0/4]], 10))
+            np.testing.assert_array_equal(np.round(z2.get_der([x]),10), np.round([[-4.0/9], [0.5]], 10))
+            np.testing.assert_array_equal(np.round(z2.get_der([y]), 10), np.round([[2.0/3], [-5.0/4]], 10))
 
         def test_rdiv():
             z1 = 10/z
-            assert np.round(z1.get_value(), 10) == np.round([10.0/3, 5.0], 10)
-            assert np.round(z1.get_der([x,y]), 4) == np.round([[-10.0/9, 0], [0, 2.5]], 10)
-            assert np.round(z1.get_der([x]),10) == np.round([[-10.0/9], [0]], 10)
-            assert np.round(z1.get_der([y]), 10) == np.round([[0], [2.5]], 10)
+            np.testing.assert_array_equal(np.round(z1.get_value(), 10), np.round([10.0/3, 5.0], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([x,y]), 10), np.round([[-10.0/9, 0], [0, -2.5]], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([x]),10), np.round([[-10.0/9], [0]], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([y]), 10), np.round([[0], [-2.5]], 10))
 
         def test_pow():
             z1 = z**2
@@ -676,16 +675,16 @@ def test_vector_vector():
             
             z2 = z**p
             assert z2.get_value() == [81.0, 32.0]
-            assert np.round(z2.get_der([x,y]), 10) == np.round([[108.0, 162.0*np.log(3)], [32.0*np.log(2), 80.0]], 10)
-            assert np.round(z2.get_der([x]),10) == np.round([[108.0], [32.0*np.log(2)]], 10)
-            assert np.round(z2.get_der([y]), 10) == np.round([[162.0*np.log(3)], [80.0]], 10)
+            np.testing.assert_array_equal(np.round(z2.get_der([x,y]), 10), np.round([[108.0, 162.0*np.log(3)], [32.0*np.log(2), 80.0]], 10))
+            np.testing.assert_array_equal(np.round(z2.get_der([x]),10), np.round([[108.0], [32.0*np.log(2)]], 10))
+            np.testing.assert_array_equal(np.round(z2.get_der([y]), 10), np.round([[162.0*np.log(3)], [80.0]], 10))
 
         def test_rpow():
             z1 = 2**z
             assert z1.get_value() == [8.0, 4.0]
-            assert np.round(z1.get_der([x,y]), 10) == np.round([[8.0*np.log(2), 0], [0, 4.0*np.log(2)]], 10)
-            assert np.round(z1.get_der([x]),10) == np.round([[8.0*np.log(2)], [0]], 10)
-            assert np.round(z1.get_der([y]), 10) == np.round([[0], [4.0*np.log(2)]], 10)
+            np.testing.assert_array_equal(np.round(z1.get_der([x,y]), 10), np.round([[8.0*np.log(2), 0], [0, 4.0*np.log(2)]], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([x]),10), np.round([[8.0*np.log(2)], [0]], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([y]), 10), np.round([[0], [4.0*np.log(2)]], 10))
             
 
         test_add()
@@ -760,10 +759,11 @@ def test_vector_vector():
             x = Var(np.pi)
             y = Var(np.pi/2)
             z = MultiFunc([x, y])
-            z1 = Var.sin(Var.cos(z))
-            assert np.round(z1.get_value(),10) == [-0.8414709848, 0]
-            assert z1.get_der([x,y]) == [[0, 0], [0, -1.0]]  # = -sin(x)cos(cos(x)) - sin(y)cos(cos(y))
-            assert z1.get_der([x]) == [[0], [0]]
+            f = z.apply(Var.cos)
+            z1 = f.apply(Var.sin)
+            np.testing.assert_array_equal(np.round(z1.get_value(),10), np.round([-0.8414709848, 0], 10))
+            np.testing.assert_array_equal(np.round(z1.get_der([x,y]),10), np.round([[0, 0], [0, -1.0]], 10))  # = -sin(x)cos(cos(x)) - sin(y)cos(cos(y))
+            np.testing.assert_array_equal(np.round(z1.get_der([x]), 10), np.round([[0], [0]], 10))
             assert z1.get_der([y]) == [[0], [-1.0]]
         
         def test_2():
@@ -772,9 +772,9 @@ def test_vector_vector():
             z = MultiFunc([x, y])  
             z1 = z**2+2
             assert z1.get_value() == [3.0, 6.0]    
-            assert z1.get_der([x,y]) == [[4.0,0], [0, 6.0]]
-            assert z1.get_der([x]) == [[4.0], [0]]
-            assert z1.get_der([y]) == [[0], [6.0]]
+            assert z1.get_der([x,y]) == [[2.0,0], [0, 4.0]]
+            assert z1.get_der([x]) == [[2.0], [0]]
+            assert z1.get_der([y]) == [[0], [4.0]]
               
         test_1_trig()
         test_2()
